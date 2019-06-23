87e537960be76eb5a3de82d8c1a8ea07315ec9d7
Hacker Destination <hackerdestination04@gmail.com>
2019-06-23 21:20:03 +0530

    Update

--- darkscrape.py
+++ darkscrape.py
@@ -1,11 +1,16 @@
-import socket
+#!/usr/bin/env python3
+from modules.file import file
+from modules.url import url
 import requests
-from bs4 import BeautifulSoup
+import subprocess as subp
 
 R = '\033[31m' # red
 G = '\033[32m' # green
 C = '\033[36m' # cyan
 W = '\033[0m' # white
+
+
+version = '1.3'
 
 def banner():
 	banner = r'''
@@ -19,35 +24,71 @@ def banner():
 	                                          |_|'''
 	print(G + banner + W)
 	print(R + "Created By :- " + G + "Hacker Destination" +W)
-	print(R + "Version :- " + G + "1.0" + W + '\n')
+	print(R + "Version :- " + G + version + W + '\n')
 
 session = requests.session()
 session.proxies = {}
 session.proxies['http'] = 'socks5h://localhost:9050'
 session.proxies['https'] = 'socks5h://localhost:9050'
 
+def updater():
+	print (G + '[+]' + R + ' Checking for updates...' + W + '\n')
+	updated_version = requests.get('https://raw.githubusercontent.com/itsmehacker/DarkScrape/master/version.txt', timeout = 5)
+	updated_version = updated_version.text.split(' ')[1]
+	updated_version = updated_version.strip()
+	if updated_version != version:
+		print (G + '[!]' + R + ' A New Version is Available : ' + W + updated_version)
+		ans = input(G + '[!]' + R + ' Update ? [y/n] : ' + W)
+		if ans == 'y':
+			print ('\n' + G + '[+]' + R + ' Updating...' + '\n' + W)
+			subp.check_output(['git', 'reset', '--hard', 'origin/master'])
+			subp.check_output(['git', 'pull'])
+			print (G + '[+]' + R + ' Script Updated...Please Execute Again...' + W)
+			exit()
+	else:
+		print (G + '[+]' + R + ' Script is up-to-date...' + '\n' + W)
+	
+
+def service():
+	print('\n' + C + "[>] Checking for tor service..." + W + '\n')
+	cmd = 'systemctl is-active tor.service'
+	co = subp.Popen(cmd, shell=True,stdout=subp.PIPE).communicate()[0]
+	if 'inactive' in str(co):
+		print(R + '[!] Tor Service Not Running..' + W + '\n')
+		print(R + '[>] Tor Service is Required for this Script...')
+		exit()
+	else:
+		print(C + "[>] Tor Service is Running..."  + W + '\n')
+
+
 
 def scrap():
 	r= session.get("http://icanhazip.com").text
 	print(R + '[+]' + G  + ' Connected to Tor...')
 	print(R + '[+]' + G  + ' Your Tor IP -> {}'.format(r))
-	print(R + '[+]' + G  + ' Starting the Website Scraping' + "\n")
-	
 
-	file = 'test.txt' #file name
-	
-	with open(file, 'r') as torfile:
-		torfile = torfile.readlines()
-		for res in torfile:
-			response = session.get(res).text
-			soup = BeautifulSoup(response, 'lxml' ,HTTPAdapter(max_retries=5))
-			tags = soup.find_all('img')
-			for tag in tags:
-				print(C + "[>] " + W + tag.get('src'))
+def main():
+	choice ='0'
+	while choice =='0':
+		print(R + '[+] ' + G +  'Choose the File Format:-' + W)
+		print(R + '[1] ' + G +  'Scrape From File'  + W)
+		print(R + '[2] ' + G +  'Scrape From Single URL'  + W + '\n')
+		choice = input (G + '[+]' + C + " Enter Option No. ->  " + W)
+
+		if choice == "2":
+			url()
+		elif choice == "1":
+			file()
+		else:
+			print('\n' + R + "[!] I don't understand your choice." + W + '\n')
+			return main()	
 
 try:
 	banner()
+	updater()
+	service()
 	scrap()
+	main()
 except KeyboardInterrupt:
 	print ('\n' + R + '[!]' + R + ' Keyboard Interrupt.' + W)
 	exit()
